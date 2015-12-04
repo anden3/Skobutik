ddebe257fcdbc426d0bea5f51975197c70e42740
André Vennberg <andre.vennberg@gmail.com>
2015-12-04 12:39:32 +0100

    added dynamic shoe display

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-$.post("get_shoes.php", {
-    "none": "none"
-}, function (data) {
-    console.log(data);
-});
+function displayShoe(data) {
+    var name = "<h3>" + data.Name + "</h3>";
+    var brand = "<p>" + data.Brand + "</p>";
+    var image = '<img src="' + data.Image + '">';
+
+    return '<div class="shoe">' + name + brand + image + "</div>";
+}
+
+function getShoes() {
+    $.post("get_shoes.php", {
+        "none": "none"
+    }, function (data) {
+        for (var s = 0; s < data.length; s++) {
+            $(".contents").append(displayShoe(data[s]));
+        }
+    });
+}
+
+getShoes();
